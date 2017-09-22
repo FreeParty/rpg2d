ebb4b7f620d93a7787e6e12f6e0749d2c2ed8d2b
peta345 <midori.no.sanae@gmail.com>
2017-09-22 17:29:36 +0900

    エンカウントしたときキャラクタを静止させる

--- rpg2d2/Assets/Resources/Scripts/EncountController.cs
+++ rpg2d2/Assets/Resources/Scripts/EncountController.cs
@@ -6,11 +6,15 @@ public class EncountController : MonoBehaviour {
 
 	GameObject fadeObj; // fade in/out を制御するオブジェクト
 	FadeinController m_fade;
+	GameObject stickUI;
+	Canvas canvas;
+
 
 	void Start()
 	{
 		fadeObj = GameObject.Find ("fade/Panel");
 		m_fade = fadeObj.GetComponent<FadeinController> ();
+		canvas = GameObject.Find("MobileSingleStickControl").GetComponent<Canvas>();
 	}
 
 	public void RandomEncount()
@@ -18,8 +22,10 @@ public class EncountController : MonoBehaviour {
 		int num = UnityEngine.Random.Range (0, 101);
 		if (num == 50) 
 		{
+			canvas.enabled = false;
 			m_fade.alfa = 0;
 			m_fade.isFadeOut = true;
+			Time.timeScale = 0;
 		}
 	}
 
