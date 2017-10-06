fb097861fbd568546d9b5010122c66895a76ecd3
peta345 <midori.no.sanae@gmail.com>
2017-10-06 17:42:53 +0900

    [デバッグ用]終了ボタン作成

---
+++ rpg2d2/Assets/Resources/Scripts/GameEnd.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnd : MonoBehaviour {
+
+
+	public void onPush(){
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#elif UNITY_WEBPLAYER
+			Application.OpenURL("http://www.yahoo.co.jp/");
+		#else
+			Application.Quit();
+		#endif
+	}
+}
