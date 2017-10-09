87e910c9111ffcc1b66f8d2821dd69203be87e51
peta345 <midori.no.sanae@gmail.com>
2017-10-09 21:09:14 +0900

    レベルアップ処理

---
+++ rpg2d2/Assets/Resources/Scripts/ExpController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpController : MonoBehaviour {
+
+	public static Dictionary<int, int> exp_table = new Dictionary<int, int> () { 
+		{1 ,1},
+		{2, 26},
+		{3, 50},
+		{4, 90},
+		{5, 130}
+	};
+}
