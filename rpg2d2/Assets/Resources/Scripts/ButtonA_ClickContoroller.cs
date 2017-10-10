0581333919bcd0267c609daf3e34b13219863787
FB <fb@flybird.jp>
2017-10-11 00:51:08 +0900

    Issue #29

--- rpg2d2/Assets/Resources/Scripts/ButtonA_ClickContoroller.cs
+++ rpg2d2/Assets/Resources/Scripts/ButtonA_ClickContoroller.cs
@@ -11,12 +11,7 @@ public class ButtonA_ClickContoroller : MonoBehaviour {
 	}
 	
 	public void onPushCheckObject(){ // call if push A button
-
-		PlayerContoroller pc = player.GetComponent<PlayerContoroller> ();
-		Debug.Log ("tag2 is " + pc.objectNameTag);
-		if(pc.objectNameTag == "StrongBox"){
-			Debug.Log ("hoge1");
-			pc.StrongBoxFlg = true;
-		}
-	}
+        PlayerContoroller pc = player.GetComponent<PlayerContoroller>();
+        pc.CheckObject();
+    }
 }
