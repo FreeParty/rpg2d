04018bf9d27ba6ddfeb16a9b5472d6136047080e
peta345 <midori.no.sanae@gmail.com>
2017-10-10 16:14:46 +0900

    se再生する機能

---
+++ rpg2d2/Assets/Resources/Scripts/BattleSoundsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSoundsController : MonoBehaviour {
+	public AudioClip PlayerAttack;
+	public AudioClip Lvup;
+	public AudioClip audioClip3;
+	private AudioSource audioSource;
+
+	public void Attack(){
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.clip = PlayerAttack;
+		audioSource.Play ();
+	}
+
+	public void LvUp(){
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.clip = Lvup;
+		audioSource.Play ();
+	}
+}
