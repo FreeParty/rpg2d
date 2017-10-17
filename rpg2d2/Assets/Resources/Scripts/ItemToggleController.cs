c082e33e426974505f86bdf8bf573418d2b9a75e
FB <fb@flybird.jp>
2017-10-18 00:29:17 +0900

    #48 #49
    アイテムを使う（メイン画面のみ）
    現時点では回復アイテムのみ、使うとステータスを更新

---
+++ rpg2d2/Assets/Resources/Scripts/ItemToggleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemToggleController : MonoBehaviour {
+
+    public int itemNo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Select()
+    {
+        GameObject.Find("Description").GetComponentInChildren<Text>().text = "【" + ItemList.item_table[itemNo].item_name + "】\n" + ItemList.item_table[itemNo].item_desc;
+    }
+}
