6f69d40292a077a2b0459423e3913ff6d1dd562d
murakami0105 <14ec105@ms.dendai.ac.jp>
2017-10-25 19:49:57 +0900

    #13 タイトルのボタンに効果音を付ける

--- rpg2d2/Assets/Resources/Scripts/TitleController.cs
+++ rpg2d2/Assets/Resources/Scripts/TitleController.cs
@@ -7,6 +7,8 @@ using System;
 public class TitleController : MonoBehaviour
 {
     private string firstSceneName = "map_station";
+    AudioSource audio;
+
     // Use this for initialization
     void Start()
     {
@@ -21,16 +23,22 @@ public class TitleController : MonoBehaviour
     public void NewGameCallback(string playerName)
     {
         PlayerContoroller.player_name = playerName;
+	audio = GetComponent<AudioSource>();
+	audio.PlayOneShot(audio.clip);
         GameObject.Find("GameManager").GetComponent<GameManager>().SceneChange(firstSceneName,true);
     }
 
     public void NewGame()
     {
+	audio = GetComponent<AudioSource>();
+	audio.PlayOneShot(audio.clip);
         AlertController.alertController.ShowAlertByInput("ニューゲーム", "名前を入力してください。", "ゆうしゃ" ,NewGameCallback);
     }
 
     public void Continue()
     {
+	audio = GetComponent<AudioSource>();
+	audio.PlayOneShot(audio.clip);
         GameObject.Find("GameManager").GetComponent<GameManager>().Load();
     }
 }
