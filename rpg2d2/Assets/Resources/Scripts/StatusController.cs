308212c39dd50b94b01191e56f910911daf2ce01
FB <fb@flybird.jp>
2017-10-30 15:45:48 +0900

    bug fix

--- rpg2d2/Assets/Resources/Scripts/StatusController.cs
+++ rpg2d2/Assets/Resources/Scripts/StatusController.cs
@@ -20,8 +20,8 @@ public class StatusController : MonoBehaviour {
         if (gameObject.activeSelf) {
             GameObject.Find("Name").GetComponent<Text>().text = PlayerContoroller.player_name;
             GameObject.Find("Lv").GetComponent<Text>().text = "LV : " + PlayerContoroller.player_status["lv"];
-            GameObject.Find("Hp").GetComponent<Text>().text = "HP : " + PlayerContoroller.player_status["hp"] + " / " + PlayerContoroller.player_status["mhp"];
-            GameObject.Find("Mp").GetComponent<Text>().text = "MP : " + PlayerContoroller.player_status["mp"] + " / " + PlayerContoroller.player_status["mmp"];
+            GameObject.Find("Hp").GetComponent<Text>().text = "HP : " + Mathf.Max(PlayerContoroller.player_status["hp"],0) + " / " + PlayerContoroller.player_status["mhp"];
+            GameObject.Find("Mp").GetComponent<Text>().text = "MP : " + Mathf.Max(PlayerContoroller.player_status["mp"],0) + " / " + PlayerContoroller.player_status["mmp"];
         }
     }
 }
